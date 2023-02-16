c8c54b544bc86e13e4f37479268b83843de8c22f
PercyPotterN <111822433+PercyPotterN@users.noreply.github.com>
2023-02-15 19:43:23 -0800

    Add files via upload
    Fixed case-sensitive search

--- main.js
+++ main.js
@@ -57,11 +57,12 @@ function sorting() {
 
 
 function searching() {
-	var s = document.getElementById("s1").value;
+	var s = document.getElementById("s1").value.toLowerCase().trim();
 	var found = 0;
 	var j;
+
 	for (j = 0; j < names_of_people.length; j++) {
-		if (s == names_of_people[j]) {
+		if (s == names_of_people[j].toLowerCase().trim()) {
 			found = found + 1;
 		}
 	}
